28691c3b27cca427dea51f282984334692d147f8
JoseA <josealejandro.garcia@hotmail.com>
2021-08-29 20:29:35 -0500

    working on adding the second report

---
+++ Department_of_Statistics/Window2.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+//using LiveCharts;
+//using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+namespace Department_of_Statistics
+{
+    /// <summary>
+    /// Lógica de interacción para Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public List<int> muns;
+        public List<string> dpts;
+        public Window2()
+        {
+            InitializeComponent();
+            fillChart();
+        }
+
+        private void fillChart()
+        {
+            
+            var myValues = new LiveCharts.ChartValues<int>(muns);
+            PieSeries ps = new PieSeries(myValues);
+            //pieChart.ChartLegend.SetCurrentValue(dpts);
+        }
+
+        internal void setInfo(Country country)
+        {
+            muns = country.getMcpAmounts();
+            dpts = country.getDptNames();
+
+        }
+
+    }
+}
